86a568e34d42be515fb308e13f365ea21da6e45a
Luiz-Fellipe <devluizfellipe@gmail.com>
2020-02-02 11:39:48 -0300

    configurando ferramentas

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dealect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'fastfeet',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
