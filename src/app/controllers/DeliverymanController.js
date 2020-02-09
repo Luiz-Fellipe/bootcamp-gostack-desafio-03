58e6f51ef5d9543e6a9f18c551cf63e986627968
Luiz-Fellipe <devluizfellipe@gmail.com>
2020-02-09 19:26:30 -0300

    fazendo CRUD de entregadores

---
+++ src/app/controllers/DeliverymanController.js
@@ -0,0 +1,91 @@
+import * as Yup from 'yup';
+import Deliveryman from '../models/Deliveryman';
+
+class DeliverymanController {
+  async index(req, res) {
+    const deliverymen = await Deliveryman.findAll();
+    return res.json(deliverymen);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validations fails' });
+    }
+
+    const deliveryExist = await Deliveryman.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (deliveryExist) {
+      return res.status(400).json('Delivery already exist');
+    }
+
+    const newDeliveryman = await Deliveryman.create(req.body);
+
+    return res.status(201).json(newDeliveryman);
+  }
+
+  async show(req, res) {
+    const deliveryman = await Deliveryman.findByPk(req.params.id);
+    if (!deliveryman) {
+      return res.status(400).json({ error: "deliveryman don't exist" });
+    }
+    return res.json(deliveryman);
+  }
+
+  async update(req, res) {
+    const deliveryman = await Deliveryman.findByPk(req.params.id);
+    if (!deliveryman) {
+      return res.status(400).json({ error: "deliveryman don't exist" });
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validations fails' });
+    }
+
+    // verificando se o email ja não existe
+    if (req.body.email && deliveryman.email !== req.body.email) {
+      const deliveryExist = await Deliveryman.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
+      if (deliveryExist) {
+        return res.status(400).json('Deliveryman already exist');
+      }
+    }
+
+    const newDeliveryman = await deliveryman.update(req.body);
+
+    return res.json(newDeliveryman);
+  }
+
+  async delete(req, res) {
+    const deliveryman = await Deliveryman.findByPk(req.params.id);
+    if (!deliveryman) {
+      return res.status(400).json({ error: "deliveryman don't exist" });
+    }
+
+    await Deliveryman.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    return res.send();
+  }
+}
+
+export default new DeliverymanController();
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
+import DeliverymanController from './app/controllers/DeliverymanController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -15,5 +16,11 @@ routes.use(authMiddleware);
 
 routes.post('/recipients', RecipientController.store);
 routes.put('/recipients/:id', RecipientController.update);
+
+routes.get('/deliverymen', DeliverymanController.index);
+routes.get('/deliverymen/:id', DeliverymanController.show);
+routes.post('/deliverymen', DeliverymanController.store);
+routes.put('/deliverymen/:id', DeliverymanController.update);
+routes.delete('/deliverymen/:id', DeliverymanController.delete);
 
 export default routes;
